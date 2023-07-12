d3f92df6c65bcd982b42a9d552a23c56539d7175
Luis <luisernestochiquitovera@gmail.com>
2023-07-12 09:06:38 -0500

    feat(rutas): Rutas del proyecto agregadas

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'login'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/autores/autores-routing.module.ts
+++ src/app/autores/autores-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ListadoAutoresComponent } from './pages/listado-autores/listado-autores.component';
+import { ObrasFavoritasComponent } from './pages/obras-favoritas/obras-favoritas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'listado',
+        component: ListadoAutoresComponent,
+      },
+      {
+        path: 'favoritos',
+        component: ObrasFavoritasComponent,
+      },
+      {
+        path: '**',
+        component: HomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
